4b09bc5f2fcddf01f4823fcd617ce04215210a57
Abdelilah <abdelilah.khossan@gmail.com>
2026-08-25 17:36:16 +0100

    chore: tidy Product entity
    Add NORMAL/SEASONAL/EXPIRABLE type constants and remove the unused
    adjust_availability_raw raw-SQL method (dead code that bypassed the ORM).

--- orders/entities/product.py
+++ orders/entities/product.py
@@ -1,6 +1,10 @@
-from django.db import models, connection
+from django.db import models
 
 class Product(models.Model):
+    NORMAL = "NORMAL"
+    SEASONAL = "SEASONAL"
+    EXPIRABLE = "EXPIRABLE"
+
     name              = models.CharField(max_length=100)
     type              = models.CharField(max_length=20)
     available         = models.IntegerField(default=0)
@@ -9,11 +13,5 @@ class Product(models.Model):
     season_start_date = models.DateField(null=True, blank=True)
     season_end_date   = models.DateField(null=True, blank=True)
 
-    def adjust_availability_raw(self, new_amount):
-        with connection.cursor() as cursor:
-            cursor.execute(
-                "UPDATE orders_product SET available = %s WHERE id = %s",
-                [new_amount, self.id]
-            )
     class Meta:
         app_label = 'orders'
